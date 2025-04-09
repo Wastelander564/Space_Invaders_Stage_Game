8cafa12bce6b5e8f9e242c17bea729d7657f93b8
Justin van Vliet <150010282+Wastelander564@users.noreply.github.com>
2025-04-09 16:32:46 +0200

    Update web build with correct base href

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "336de13e3f920df22e6a37c80241e6d5
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "70f25d8dc363d4b7169d07f56c2088d8",
+"flutter_bootstrap.js": "b18b050e4b632594e9a4a05d9e712d70",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
